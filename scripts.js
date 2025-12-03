bde6126efe8e384f23255a81c4bc22ae3e625ec7
GabrielMendes <gabrielmendes.dev@outlook.com>
2025-12-03 13:34:03 -0300

    Meu Primeiro Projeto

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+let imagem = document.querySelector(".copao")
+let fundo = document.querySelector (".circulo")
+
+function trocaImagem(endereco){
+   imagem.src = endereco
+}
+
+function trocafundo (cor) {
+
+   fundo.style.background = cor
+}
+
+
+
